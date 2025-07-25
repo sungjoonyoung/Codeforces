2ad884050e49c3323ab0eab268bf3445b1ee0252
Joonyoung Sung Desktop <sungso376@gmail.com>
2025-07-25 18:10:48 +0900

    2114

--- 2114/2114F.cpp
+++ 2114/2114F.cpp
@@ -3,35 +3,43 @@
 #include<utility>
 #include<algorithm>
 #include<queue>
+#include<cmath>
 #define ll long long
 using namespace std;
-ll gcd(ll x, ll y){
+int dp[1'000'001];
+int gcd(int x, int y){
     if(y==0)return x;
     return gcd(y,x%y);
+}
+int dfs(int x, int K){
+    if(x<=K){
+        dp[x]=1;
+        return 1;
+    }
+    int minnum=2'000'000'000;
+    if(dp[x]!=minnum)return dp[x];
+
+    for(int i=2;i<=min((int)sqrt(x),K);i++){
+        if(x%i==0)minnum=min(minnum,dfs(x/i,K)+1);
+    }
+    //if(minnum==2'000'000'000)minnum=1;
+    dp[x]=min(dp[x],minnum);
+    return dp[x];
 }
 void solve(void){
     ll x,y,k;
     cin>>x>>y>>k;
+    fill(dp,dp+max(x,y)+1,2'000'000'000);
     if(x<y)swap(x,y);
     ll gcdnum=gcd(x,y);
     x/=gcdnum;
     y/=gcdnum;
-    int answer=0;
-    // cout<<x<<" ";
-    for(int i=k;i>=2;i--){
-        while(x%i==0){
-            cout<<x<<" "<<i<<"\n";
-            x/=i;
-            answer++;
-        }
-        while(y%i==0){
-            y/=i;
-            answer++;
-        }
-    }
+    dfs(x,k);
+    dfs(y,k);
+    dp[1]=0;
     // cout<<x<<" "<<y<<" ";
-    if(x==1 and y==1)cout<<answer<<"\n";
-    else cout<<"-1\n";
+    if(dp[x]==2'000'000'000 or dp[y]==2'000'000'000)cout<<"-1\n";
+    else cout<<dp[x]+dp[y]<<"\n";
 }
 int main(void){
     ios::sync_with_stdio(0);
